f2d31100cb022a2e7870376849c4f64467a75e08
shivani <shivani161208@gmail.com>
2026-07-08 10:12:05 +0530

    Time: 45 ms (50.26%), Space: 155.1 MB (67.36%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -3,75 +3,41 @@ public:
     const int MOD = 1000000007;
 
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-
         int n = s.size();
-
-        // Prefix sum of digits
-        vector<long long> sum(n);
-        sum[0] = s[0] - '0';
-
-        for (int i = 1; i < n; i++)
-            sum[i] = sum[i - 1] + (s[i] - '0');
-
-        // Store non-zero digits and their indices
-        vector<int> idx;
-        vector<int> digit;
+        vector<long long> prefixVal(n + 1, 0);
+        vector<int> cnt(n + 1, 0);
+        vector<long long> sumDigits(n + 1, 0);
 
         for (int i = 0; i < n; i++) {
-            if (s[i] != '0') {
-                idx.push_back(i);
-                digit.push_back(s[i] - '0');
+            int d = s[i] - '0';
+            if (d != 0) {
+                prefixVal[i + 1] = (prefixVal[i] * 10 + d) % MOD;
+                cnt[i + 1] = cnt[i] + 1;
+            } else {
+                prefixVal[i + 1] = prefixVal[i];
+                cnt[i + 1] = cnt[i];
             }
+            sumDigits[i + 1] = sumDigits[i] + d;
         }
 
-        int m = digit.size();
-
-        // 10^i % MOD
-        vector<long long> pow10(m + 1, 1);
-        for (int i = 1; i <= m; i++)
+        // precompute powers of 10
+        vector<long long> pow10(n + 1);
+        pow10[0] = 1;
+        for (int i = 1; i <= n; i++)
             pow10[i] = (pow10[i - 1] * 10) % MOD;
 
-        // Prefix number
-        vector<long long> num(m);
-
-        if (m > 0) {
-            num[0] = digit[0];
-
-            for (int i = 1; i < m; i++)
-                num[i] = (num[i - 1] * 10 + digit[i]) % MOD;
-        }
-
         vector<int> ans;
+        ans.reserve(queries.size());
 
-        for (auto q : queries) {
+        for (const auto& q : queries) {   // reference, no copy
+            int l = q[0], r = q[1];
 
-            int l = q[0];
-            int r = q[1];
+            int c = cnt[r + 1] - cnt[l];  // O(1) count, no binary search
 
-            // Digit sum
-            long long digitSum = sum[r];
-            if (l > 0)
-                digitSum -= sum[l - 1];
+            long long x = (prefixVal[r + 1] - (prefixVal[l] * pow10[c]) % MOD + MOD) % MOD;
+            long long digitSum = sumDigits[r + 1] - sumDigits[l];
 
-            // Find first and last non-zero digit
-            int first = lower_bound(idx.begin(), idx.end(), l) - idx.begin();
-            int last = upper_bound(idx.begin(), idx.end(), r) - idx.begin() - 1;
-
-            if (first > last) {
-                ans.push_back(0);
-                continue;
-            }
-
-            int len = last - first + 1;
-
-            long long x;
-
-            if (first == 0)
-                x = num[last];
-            else
-                x = (num[last] - (num[first - 1] * pow10[len]) % MOD + MOD) % MOD;
-
-            ans.push_back((x * digitSum) % MOD);
+            ans.push_back((int)((x * digitSum) % MOD));
         }
 
         return ans;

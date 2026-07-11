df724e5807559d70f0e7f8f399607d4c412d7852
shivani <shivani161208@gmail.com>
2026-07-11 14:04:48 +0530

    Time: 1 ms (20.17%), Space: 13 MB (67.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,20 @@
-class Solution {
-public: 
-    
-    int minimumTotal(vector<vector<int>>& triangle) {
+class Solution{
+public:
+    int minimumTotal(vector<vector<int>>&triangle){
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<int>front(n,0);
         for(int i=n-1;i>=0;i--){
+            vector<int>curr(n,0);
             for(int j=0;j<=i;j++){
-                if(i == n-1) dp[i][j] = triangle[i][j];
+                if(i == n-1) curr[j] = triangle[i][j];
                 else{
-                    int down = dp[i+1][j];
-                    int dia = dp[i+1][j+1];
-                    dp[i][j] = triangle[i][j] + min(down,dia);
+                    int down = front[j];
+                    int diag = front[j+1];
+                    curr[j] = triangle[i][j] + min(down,diag);
                 }
             }
+            front = curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 };

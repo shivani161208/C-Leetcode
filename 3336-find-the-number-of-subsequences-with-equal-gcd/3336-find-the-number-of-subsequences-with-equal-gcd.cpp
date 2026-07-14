b2b93123eafc97dbb00a89911e572c669794bce7
shivani <shivani161208@gmail.com>
2026-07-14 09:15:19 +0530

    Time: 483 ms (72.17%), Space: 296.2 MB (26.09%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
     const int MOD = 1e9 + 7;
-    int f(int idx ,int gcd1, int gcd2, vector<int>&nums,vector<vector<vector<int>>>&dp){
-        if(idx == nums.size()){
-            if(gcd1 == gcd2 && gcd1 != 0)  return 1;
-            return 0;
-        }
-        if(dp[idx][gcd1][gcd2] != -1) return dp[idx][gcd1][gcd2];
-        int take1 = f(idx+1, gcd(gcd1,nums[idx]),gcd2,nums,dp);
-        int take2 = f(idx+1,gcd1,gcd(gcd2,nums[idx]),nums,dp);
-        int skip = f(idx+1, gcd1, gcd2,nums,dp);
 
-        return dp[idx][gcd1][gcd2] = (1LL * take1 + take2 + skip) % MOD ;
-    }
     int subsequencePairCount(vector<int>& nums) {
         int n = nums.size();
-        int mx = *max_element(nums.begin(),nums.end());
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(mx+1,vector<int>(mx+1,-1)));
-        return f(0,0,0,nums,dp);
+        int mx = *max_element(nums.begin(), nums.end());
+
+        vector<vector<vector<int>>> dp(
+            n + 1,
+            vector<vector<int>>(mx + 1, vector<int>(mx + 1, 0))
+        );
+
+        dp[0][0][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int g1 = 0; g1 <= mx; g1++) {
+                for (int g2 = 0; g2 <= mx; g2++) {
+
+                    if (dp[i][g1][g2] == 0) continue;
+
+                    long long ways = dp[i][g1][g2];
+
+                    // Skip
+                    dp[i + 1][g1][g2] =
+                        (dp[i + 1][g1][g2] + ways) % MOD;
+
+                    // Put in first subsequence
+                    int ng1 = std::gcd(g1, nums[i]);
+                    dp[i + 1][ng1][g2] =
+                        (dp[i + 1][ng1][g2] + ways) % MOD;
+
+                    // Put in second subsequence
+                    int ng2 = std::gcd(g2, nums[i]);
+                    dp[i + 1][g1][ng2] =
+                        (dp[i + 1][g1][ng2] + ways) % MOD;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int g = 1; g <= mx; g++) {
+            ans = (ans + dp[n][g][g]) % MOD;
+        }
+
+        return ans;
     }
 };

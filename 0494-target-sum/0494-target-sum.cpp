8ec22143449514ca3ce6771a8f09ea75fc99ace0
shivani <shivani161208@gmail.com>
2026-07-17 22:06:32 +0530

    Time: 3 ms (85.6%), Space: 15.4 MB (48.87%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int f(int idx,int sum,vector<int>&nums, int tar){
-        if(idx == nums.size()){
-            if(sum == tar) return 1;
+    int f(int idx,int sum,vector<int>&nums,vector<vector<int>>&dp){
+        if(idx == 0){
+            if(sum == 0 && nums[0] == 0) return 2;
+            if(sum == 0 || sum == nums[0]) return 1;
             return 0;
         }
-        int plus = f(idx+1,sum + nums[idx],nums,tar);
-        int minus = f(idx+1,sum - nums[idx],nums,tar);
-        return plus+minus;
+        if(dp[idx][sum] != -1) return dp[idx][sum];
+        int ntTake = f(idx-1,sum,nums,dp);
+        int take = 0;
+        if(nums[idx] <= sum)
+            take = f(idx-1,sum - nums[idx],nums,dp);
+
+        return dp[idx][sum] = take + ntTake;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(0,0,nums,target);
+        int tot = 0;
+        for(int x:nums) tot += x;
+        if(abs(target) > tot) return 0;
+        if((tot + target) % 2) return 0;
+        int ans = (tot + target) / 2;
+        int n = nums.size();
+        vector<vector<int>>dp(n,vector<int>(ans+1,-1));
+        return f(n-1,ans,nums,dp);
     }
 };

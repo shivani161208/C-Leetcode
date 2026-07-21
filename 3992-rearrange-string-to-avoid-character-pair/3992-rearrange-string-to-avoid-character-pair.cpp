58a494277c66cbe24a9a40c4330eec000419e78c
shivani <shivani161208@gmail.com>
2026-07-21 08:59:00 +0530

    Time: 0 ms (100%), Space: 10.2 MB (31.71%) - LeetHub

--- 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     string rearrangeString(string s, char x, char y) {
-        string xc = "";
-        string yc ="";
-        string other = "";
+        string xc,yc,other;
+        int n = s.size();
+        xc.reserve(n);
+        yc.reserve(n);
+        other.reserve(n);
 
-        for(char c : s){
-            if(c == y) yc += c;
-            else if(c == x) xc +=c;
-            else other += c;
+        for(char c :s){
+            if(c == y) yc.push_back(c);
+            else if(c == x) xc.push_back(c);
+            else other.push_back(c);
         }
         return yc + other + xc;
     }

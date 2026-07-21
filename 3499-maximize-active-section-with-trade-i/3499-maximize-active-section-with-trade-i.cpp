b595e895c8b597205b80e8e693284f29df72d6ca
shivani <shivani161208@gmail.com>
2026-07-21 08:43:39 +0530

    Time: 88 ms (40.54%), Space: 109.8 MB (27.48%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
        
-        string t = "1" + s + "1";
+        string t;
+        t.reserve(s.size()+2);
+        t.push_back('1');
+        t += s;
+        t.push_back('1');
         int n = t.length();
         vector<pair<char,int>> seg;
+        seg.reserve(t.size());
         int i = 0;
+        int ones = 0;
         while(i < n){
             char ch = t[i];
             int cnt = 0;
             while(i<n && t[i] == ch){
+                if(ch == '1' && i>0 && i < n-1) ones++;
                 cnt++;
                 i++;
             }
@@ -17,14 +24,11 @@ public:
             
         }
         int delta = 0;
-        for(int i=1;i+1<seg.size();i++){
+        int m = seg.size();
+        for(int i=1;i<m-1;i++){
             if(seg[i].first == '1' && seg[i-1].first == '0' && seg[i+1].first == '0'){
                 delta = max(delta,seg[i-1].second + seg[i+1].second);
             }
-        }
-        int ones = 0;
-        for(char ch:s){
-            if(ch == '1') ones++;
         }
         return ones + delta;
     }

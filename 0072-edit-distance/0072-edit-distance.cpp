97d0f0b1750f8f5beff15f327abc40350229c5d9
shivani <shivani161208@gmail.com>
2026-07-24 22:58:46 +0530

    Time: 0 ms (100%), Space: 10.4 MB (96.95%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int f(int i,int j,string &word1, string &word2,vector<vector<int>>&dp){
-        if(i == 0) return j;
-        if(j == 0) return i;
-        if(dp[i-1][j-1] != -1) return dp[i-1][j-1];
-        if(word1[i-1] == word2[j-1]){
-            return dp[i-1][j-1] = 0 + f(i-1,j-1,word1,word2,dp);
-        }
-        return dp[i-1][j-1] = 1 + min(f(i-1,j,word1,word2,dp),
-                    min(f(i,j-1,word1,word2,dp),f(i-1,j-1,word1,word2,dp)));
-    }
+    
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,word1,word2,dp);
+        vector<int>prev(m+1,0),cur(m+1,0);
+        for(int j=0;j<=m;j++){
+            prev[j] = j;
+        }
+        
+        for(int i=1;i<=n;i++){
+            cur[0] = i;
+            for(int j=1;j<=m;j++){
+                if(word1[i-1] == word2[j-1]){
+                    cur[j] = 0 + prev[j-1];
+                }
+                else{
+                    cur[j] = 1 + min(prev[j],
+                            min(cur[j-1],prev[j-1]));
+                } 
+            }
+            prev = cur;
+        }
+        return prev[m];
     }
 };

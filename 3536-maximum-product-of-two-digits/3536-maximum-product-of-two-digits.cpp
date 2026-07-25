b51c087c4af727e72984cc8c0e0e0b13ad2a21de
shivani <shivani161208@gmail.com>
2026-07-25 08:04:02 +0530

    Time: 0 ms (100%), Space: 9.4 MB (10.11%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -3,8 +3,7 @@ public:
     int maxProduct(int n) {
         string s = to_string(n);
         sort(s.begin(),s.end());
-        int ans = 0;
-        ans = (s[s.size()-1] - '0')*(s[s.size()-2] - '0');
+        int ans = (s[s.size()-1] - '0')*(s[s.size()-2] - '0');
         return ans;
     }
 };

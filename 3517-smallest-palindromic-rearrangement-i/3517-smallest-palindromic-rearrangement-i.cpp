c10fac5af93e3e92888a1e6f7280168ab751953e
shivani <shivani161208@gmail.com>
2026-07-28 08:46:05 +0530

    Time: 18 ms (91.43%), Space: 61 MB (78.46%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        int n = s.size();
-        vector<int>freq(26,0);
-        for(char ch : s){
+        int freq[26] = {0};
+
+        for (char ch : s)
             freq[ch - 'a']++;
+
+        int n = s.size();
+        string ans(n, ' ');
+
+        int l = 0, r = n - 1;
+
+        // Fill pairs from smallest character
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] > 1) {
+                ans[l++] = char(i + 'a');
+                ans[r--] = char(i + 'a');
+                freq[i] -= 2;
+            }
         }
-        string left="";
-        char mid = '\0';
 
-        for(int i=0;i<26;i++){
-            left += string(freq[i]/2, char(i + 'a'));
-
-            if(freq[i]%2)
-               mid = char(i + 'a');
+        // Place the middle character (if any)
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 1) {
+                ans[l] = char(i + 'a');
+                break;
+            }
         }
-        string right = left;
-        reverse(right.begin(),right.end());
-        if(mid != '\0')
-            return left + mid + right;
 
-        return left + right;
+        return ans;
     }
 };

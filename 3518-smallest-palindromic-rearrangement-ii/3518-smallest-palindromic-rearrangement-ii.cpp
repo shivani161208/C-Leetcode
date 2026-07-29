b69b7267e3648dd98f11d015a38f36dc07275b26
shivani <shivani161208@gmail.com>
2026-07-29 18:26:53 +0530

    Time: 31 ms (47.83%), Space: 18 MB (24.64%) - LeetHub

---
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -0,0 +1,91 @@
+class Solution {
+public:
+    const long long LIMIT = 1000000;
+
+    // Compute C(n, r) with cap at LIMIT+1
+    long long combCapped(long long n, long long r) {
+        if (r < 0 || r > n) return 0;
+        r = min(r, n - r);
+        if (r == 0) return 1;
+
+        __int128 res = 1;
+        for (long long i = 1; i <= r; i++) {
+            res = res * (n - r + i) / i;
+            if (res > LIMIT) return LIMIT + 1;
+        }
+        return (long long)res;
+    }
+
+    // Count permutations of multiset with frequencies cnt[], capped at LIMIT+1
+    long long cntPer(vector<int>& cnt, int /*len*/, long long /*limit*/) {
+        long long remaining = 0;
+        for (int x : cnt) remaining += x;
+
+        long long ans = 1;
+        for (int c : cnt) {
+            if (c == 0) continue;
+            long long part = combCapped(remaining, c);
+            ans *= part;
+            if (ans > LIMIT) return LIMIT + 1;
+            remaining -= c;
+        }
+        return ans;
+    }
+
+    string smallestPalindrome(string s, int k) {
+        vector<int> freq(26, 0);
+        for (char ch : s)
+            freq[ch - 'a']++;
+
+        int odd = 0;
+        char mid = '#';
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2) {
+                odd++;
+                mid = char(i + 'a');
+            }
+        }
+        if (odd > 1) return "";
+
+        vector<int> halfFreq(26, 0);
+        int halfLen = 0;
+        for (int i = 0; i < 26; i++) {
+            halfFreq[i] = freq[i] / 2;
+            halfLen += halfFreq[i];
+        }
+
+        long long K = k;
+        long long tot = cntPer(halfFreq, halfLen, LIMIT);
+        if (K > tot) return "";
+
+        string l = "";
+        int rem = halfLen;
+
+        for (int pos = 0; pos < halfLen; pos++) {
+            for (int c = 0; c < 26; c++) {
+                if (halfFreq[c] == 0) continue;
+
+                halfFreq[c]--;
+                rem--;
+
+                long long ways = cntPer(halfFreq, rem, K);
+                if (ways >= K) {
+                    l += char(c + 'a');
+                    break;
+                } else {
+                    K -= ways;
+                    halfFreq[c]++;
+                    rem++;
+                }
+            }
+        }
+
+        string ans = l;
+        if (mid != '#') ans += mid;
+
+        reverse(l.begin(), l.end());
+        ans += l;
+        return ans;
+    }
+};

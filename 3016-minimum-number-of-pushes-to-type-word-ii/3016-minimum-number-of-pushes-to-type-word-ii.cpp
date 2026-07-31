5e0db477523dd4c38eba1110249667cc1dd9900b
shivani <shivani161208@gmail.com>
2026-07-31 09:06:35 +0530

    Time: 30 ms (24.96%), Space: 26.8 MB (51.64%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n = word.size();
         unordered_map<char,int>freq;
         for(char ch : word) freq[ch]++;
-        vector<pair<int,char>>p;
-        for(auto it:freq){
-            p.push_back({it.second,it.first});
-        }
-        sort(p.begin(),p.end(),greater<pair<int,char>>());
+        vector<int>counts;
+        for(auto it:freq)
+            counts.push_back(it.second);
+        
+        sort(counts.begin(),counts.end(),greater<int>());
 
         int ans = 0;
-        for(int i=0;i<p.size();i++){
-            int cnt = (i/8) + 1;
-            ans += p[i].first *cnt;
-        }
-     return ans;
+        for(int i=0;i<counts.size();i++)
+            ans += counts[i] * (i / 8 + 1);
+        
+         return ans;
     }
 };

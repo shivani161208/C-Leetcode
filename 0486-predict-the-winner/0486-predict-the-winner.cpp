1e0a20d3fae207844caea9caa164ce263768481d
shivani <shivani161208@gmail.com>
2026-08-01 09:11:02 +0530

    Time: 0 ms (100%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,16 +3,12 @@ public:
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int i=0;i<n;i++) dp[i][i] = nums[i];
-        
+        vector<int>dp(nums);        
         for(int i=n-1;i>=0;i--){
             for(int j=i+1;j<n;j++){
-                int left = nums[i] - dp[i+1][j];
-                int right = nums[j] - dp[i][j-1];
-                dp[i][j] = max(left,right);
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j-1]);
             }
         }
-        return dp[0][n-1] >= 0;
+        return dp[n-1] >= 0;
     }
 };

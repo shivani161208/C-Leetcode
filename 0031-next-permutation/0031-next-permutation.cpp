cb80006dd042a79bbfe40c64482d1eac870491f3
shivani <shivani161208@gmail.com>
2026-08-01 10:04:39 +0530

    Time: 0 ms (100%), Space: 15.6 MB (77.21%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int piv = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                piv = i;
+                break;
+            }
+        }
+        if(piv != -1){
+            for(int i=n-1;i>=piv;i--){
+                if(nums[i] > nums[piv]){
+                    swap(nums[i],nums[piv]);
+                    break;
+                }
+            }
+        }
+        reverse(nums.begin()+piv+1,nums.end());
+    }
+};

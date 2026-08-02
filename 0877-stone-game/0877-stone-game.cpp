b28f936e721f85ed54533f6b4e038ebfaae99b48
shivani <shivani161208@gmail.com>
2026-08-02 09:29:27 +0530

    Time: 7 ms (35.23%), Space: 19.5 MB (31.69%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int f(int i,int j,vector<int>&piles,vector<vector<int>>&dp){
-        if(i == j) return piles[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        int tk = piles[i] - f(i+1,j,piles,dp);
-        int nt = piles[j] - f(i,j-1,piles,dp);
-
-        return dp[i][j] = max(tk , nt);
-    }
+   
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(0,n-1,piles,dp) > 0;
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            dp[i][i] = piles[i];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                int tk = piles[i] - dp[i+1][j];
+                int nt = piles[j] - dp[i][j-1];
+
+                dp[i][j] = max(tk , nt);
+            }
+        }
+        return dp[0][n-1] > 0;
     }
 };

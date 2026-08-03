e3019234b5109760ac48aaea569aade4a36ef092
shivani <shivani161208@gmail.com>
2026-08-03 19:13:28 +0530

    Time: 20 ms (72.63%), Space: 130.3 MB (94.81%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& stone) {
         int n = stone.size();
-        vector<int>dp(n+3,0);
+        int dp1 = 0, dp2 = 0, dp3 = 0;
         for(int i=n-1;i>=0;i--){
-            int tk1 = stone[i] - dp[i+1];
+            int tk1 = stone[i] - dp1;
 
             int tk2 = INT_MIN;
             if(i+1 < stone.size())
-                tk2 = stone[i] + stone[i+1] - dp[i+2];
+                tk2 = stone[i] + stone[i+1] - dp2;
 
             int tk3 = INT_MIN;
             if(i+2 < stone.size())
-                tk3 = stone[i] + stone[i+1] + stone[i+2] - dp[i+3];
+                tk3 = stone[i] + stone[i+1] + stone[i+2] - dp3;
 
-            dp[i] = max({tk1,tk2,tk3});
+            int curr = max({tk1,tk2,tk3});
+
+            dp3 = dp2;
+            dp2 = dp1;
+            dp1 = curr;
         }
-        int ans = dp[0];
-        if(ans > 0) return "Alice";
-        if(ans < 0) return "Bob";
+        
+        if(dp1 > 0) return "Alice";
+        if(dp1 < 0) return "Bob";
         return "Tie";
     }
 };
